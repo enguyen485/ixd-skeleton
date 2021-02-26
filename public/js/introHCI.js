49437aba10ce8a17eb8bc304ea66d4a54d2cc8d8
Nandini Kapa <nandinikapa@Nandinis-MacBook-Pro.local>
2021-02-25 22:20:56 -0800

    A6

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -1,4 +1,5 @@
 'use strict';
+// const data = require('../../data.json');
 
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
@@ -30,15 +31,40 @@ function initializePage() {
         $("#yes").css('color', 'black');
 	});
 
-    if($('#searchbar').val() == '') {
-        document.querySelector("#viewrep").disabled = true;
-        console.log("empty");
-    }
-    else {
-        document.querySelector("#viewrep").disabled = false;
-        console.log("NOT empty");
-    }
+    document.getElementById("viewrep").style.display = 'none';
+    document.getElementById("searchbar").style.width = '90vw';
+    document.getElementById("myForm").style.display = "none";
+
+    $('#searchbar').keydown(function (e){
+        if(e.keyCode == 13){
+            if($('#searchbar').val() == '') {
+                document.querySelector("#viewrep").disabled = true;
+                console.log("empty");
+                document.getElementById("viewrep").style.display = 'none';
+                document.getElementById("searchbar").style.width = '90vw';
+                document.getElementById("myForm").style.display = "none";
+            }
+            else {
+                document.querySelector("#viewrep").disabled = false;
+                console.log("NOT empty");
+                document.getElementById("searchbar").style.width = '65vw';
+                document.getElementById("viewrep").style.display = 'block';
+                var search = $('#searchbar').val();
+                var address = search.split(' ').join('%20');
+                var newHTML = '<iframe id="gmap_canvas" src="https://maps.google.com/maps?q=' + address + '&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>';
+                console.log(newHTML);
+                $(".details").html(newHTML);
+            }
+        }
+    })
 }
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+    var found = 'unknown'
+    var newHTML = '<p>This feature HAS NOT been implemented yet. There is an <b>' + found + '</b> number of Positive Reports of COVID-19 at this location.</p>';
+    $("#myForm").html(newHTML);
+  }
 
 
 function altertChange() {
